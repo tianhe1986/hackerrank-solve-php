7c3ea16088e42c986770d86aed7646fd58b920b8
tianhe1986 <w1s2j3229@163.com>
2019-11-19 17:23:15 +0800

    fix
    Signed-off-by: tianhe1986 <w1s2j3229@163.com>

--- Quadrant-Queries/solve.php
+++ Quadrant-Queries/solve.php
@@ -14,9 +14,10 @@ function quadrants($p, $queries) {
     $lazyFlag = [];
     
     // 创建线段树
-    buildSegTree($segTree, $originalArr, 1, 1, $n);
+    buildSegTree($segTree, $lazyFlag, $originalArr, 1, 1, $n);
     
     // 处理每个查询
+    $str = "";
     foreach ($queries as $query) {
         $item = explode(" ", $query);
         if ($item[0] == 'X') { // 沿X轴翻转
@@ -25,9 +26,12 @@ function quadrants($p, $queries) {
             updateSegTree($segTree, $lazyFlag, $item[1], $item[2], 2, 1, 1, $n);
         } else if ($item[0] == 'C') { // 统计区间内数量
             $result = getSegTreeRange($segTree, $lazyFlag, $item[1], $item[2], 1, 1, $n);
-            echo implode(" ", $result) . "\n";
+            $str .= implode(" ", $result) . "\n";
         }
     }
+    
+    echo $str;
+    //file_put_contents("./quadrants_out.txt", $str);
 }
 
 function getSegTreeRange(&$segTree, &$lazyFlag, $rangeLeft, $rangeRight, $root, $left, $right)
@@ -45,7 +49,7 @@ function getSegTreeRange(&$segTree, &$lazyFlag, $rangeLeft, $rangeRight, $root, 
     // 继续查询左右子区间
     $mid = intval(($left + $right)/2);
     $leftIndex = $root << 1;
-    $rightIndex = $leftIndex + 1;
+    $rightIndex = $leftIndex | 1;
 
     if ($mid >= $rangeLeft) {
         $tempResult = getSegTreeRange($segTree, $lazyFlag, $rangeLeft, $rangeRight, $leftIndex, $left, $mid);
@@ -68,7 +72,7 @@ function updateSegTree(&$segTree, &$lazyFlag, $rangeLeft, $rangeRight, $updateTy
 {
     // 如果整个包含在区间内， 设置flag， 更新当前值
     if ($left >= $rangeLeft && $right <= $rangeRight) {
-        $lazyFlag[$root][] = $updateType;
+        $lazyFlag[$root] = $lazyFlag[$root] ^ $updateType;
         changeItem($segTree, $root, $updateType);
         return;
     }
@@ -79,7 +83,7 @@ function updateSegTree(&$segTree, &$lazyFlag, $rangeLeft, $rangeRight, $updateTy
     // 继续处理左右子区间
     $mid = intval(($left + $right)/2);
     $leftIndex = $root << 1;
-    $rightIndex = $leftIndex + 1;
+    $rightIndex = $leftIndex | 1;
     // 继续递归处理左右区间
     if ($mid >= $rangeLeft) {
         updateSegTree($segTree, $lazyFlag, $rangeLeft, $rangeRight, $updateType, $leftIndex, $left, $mid);
@@ -95,69 +99,83 @@ function updateSegTree(&$segTree, &$lazyFlag, $rangeLeft, $rangeRight, $updateTy
 function pushDown(&$segTree, &$lazyFlag, $root)
 {
     // 没有flag， 什么也不用做
-    if (empty($lazyFlag[$root])) {
+    if ($lazyFlag[$root] == 0) {
         return;
     }
     
     $leftIndex = $root << 1;
-    $rightIndex = $leftIndex + 1;
+    $rightIndex = $leftIndex | 1;
     
-    foreach ($lazyFlag[$root] as $updateType) { // 标记下放到左右区间
-        changeItem($segTree, $leftIndex, $updateType);
-        $lazyFlag[$leftIndex][] = $updateType;
-        changeItem($segTree, $rightIndex, $updateType);
-        $lazyFlag[$rightIndex][] = $updateType;
-    }
+    $updateType = $lazyFlag[$root];
+    // 标记下放到左右区间
+    changeItem($segTree, $leftIndex, $updateType);
+    $lazyFlag[$leftIndex] = $lazyFlag[$leftIndex] ^ $updateType;
+    changeItem($segTree, $rightIndex, $updateType);
+    $lazyFlag[$rightIndex] = $lazyFlag[$rightIndex] ^ $updateType;
     
-    $lazyFlag[$root] = [];
+    $lazyFlag[$root] = 0;
 }
 
 function changeItem(&$segTree, $root, $updateType)
 {
-    if ($updateType == 1) { //沿x轴翻转，2和3象限交换， 1和4象限交换
-        $temp = $segTree[$root][1];
-        $segTree[$root][1] = $segTree[$root][2];
-        $segTree[$root][2] = $temp;
-        
-        $temp = $segTree[$root][0];
-        $segTree[$root][0] = $segTree[$root][3];
-        $segTree[$root][3] = $temp;
-    } else {  //沿y轴翻转，1和2象限交换， 3和4象限交换
-        $temp = $segTree[$root][0];
-        $segTree[$root][0] = $segTree[$root][1];
-        $segTree[$root][1] = $temp;
-        
-        $temp = $segTree[$root][2];
-        $segTree[$root][2] = $segTree[$root][3];
-        $segTree[$root][3] = $temp;
+    switch ($updateType) {
+        case 1:
+            $temp = $segTree[$root][1];
+            $segTree[$root][1] = $segTree[$root][2];
+            $segTree[$root][2] = $temp;
+
+            $temp = $segTree[$root][0];
+            $segTree[$root][0] = $segTree[$root][3];
+            $segTree[$root][3] = $temp;
+            break;
+        case 2:
+            $temp = $segTree[$root][0];
+            $segTree[$root][0] = $segTree[$root][1];
+            $segTree[$root][1] = $temp;
+
+            $temp = $segTree[$root][2];
+            $segTree[$root][2] = $segTree[$root][3];
+            $segTree[$root][3] = $temp;
+            break;
+        case 3:
+            $temp = $segTree[$root][0];
+            $segTree[$root][0] = $segTree[$root][2];
+            $segTree[$root][2] = $temp;
+
+            $temp = $segTree[$root][1];
+            $segTree[$root][1] = $segTree[$root][3];
+            $segTree[$root][3] = $temp;
+            break;
     }
 }
 
-function buildSegTree(&$segTree, &$originalArr, $root, $left, $right)
+function buildSegTree(&$segTree, &$lazyFlag, &$originalArr, $root, $left, $right)
 {
     if ($left == $right) { //叶节点
         $segTree[$root] = [0, 0, 0, 0];
         $segTree[$root][$originalArr[$left]] = 1;
+        $lazyFlag[$root] = 0;
         return;
     }
     
     $mid = intval(($left + $right)/2);
     $leftIndex = $root << 1;
-    $rightIndex = $leftIndex + 1;
+    $rightIndex = $leftIndex | 1;
     // 继续递归处理左右区间
-    buildSegTree($segTree, $originalArr, $leftIndex, $left, $mid);
-    buildSegTree($segTree, $originalArr, $rightIndex, $mid + 1, $right);
+    buildSegTree($segTree, $lazyFlag, $originalArr, $leftIndex, $left, $mid);
+    buildSegTree($segTree, $lazyFlag, $originalArr, $rightIndex, $mid + 1, $right);
 
     for ($i = 0; $i < 4; $i++) {
         $segTree[$root][$i] = $segTree[$leftIndex][$i] + $segTree[$rightIndex][$i];
     }
+    $lazyFlag[$root] = 0;
 }
 
 // 上推求和
 function pushUp(&$segTree, $root)
 {
     $leftIndex = $root << 1;
-    $rightIndex = $leftIndex + 1;
+    $rightIndex = $leftIndex | 1;
     
     for ($i = 0; $i < 4; $i++) {
         $segTree[$root][$i] = $segTree[$leftIndex][$i] + $segTree[$rightIndex][$i];
